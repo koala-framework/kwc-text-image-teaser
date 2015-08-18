47b40117404704cd33a48d0bed991202522d0a1d
Mario Kemetinger <mk@vivid-planet.com>
2015-08-18 10:18:06 +0200

    new Image Dimensions

--- Kwc/TextImageTeaser/Image/Component.php
+++ Kwc/TextImageTeaser/Image/Component.php
@@ -4,6 +4,27 @@ class Kwc_TextImageTeaser_Image_Component extends Kwc_Basic_Image_Component
     public static function getSettings()
     {
         $ret = parent::getSettings();
+        $ret['dimensions'] = array(
+            'default'=>array(
+                'text' => trlKwfStatic('full width'),
+                'width' => self::CONTENT_WIDTH,
+                'height' => 0,
+                'cover' => true
+            ),
+            '16to9'=>array(
+                'text' => trlKwfStatic('full width').' 16:9',
+                'width' => self::CONTENT_WIDTH,
+                'cover' => true,
+                'aspectRatio' => 9/16
+            ),
+            '4to3'=>array(
+                'text' => trlKwfStatic('full width').' 4:3',
+                'width' => self::CONTENT_WIDTH,
+                'cover' => true,
+                'aspectRatio' => 3/4
+            )
+        );
+
         $ret['imageCaption'] = true;
         return $ret;
     }
