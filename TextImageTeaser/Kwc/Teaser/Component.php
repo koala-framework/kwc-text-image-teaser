c54b267b6e961660e2ecf31ec8287f1a84c252e4
Ricky Smith <rs@vivid-planet.com>
2019-11-28 09:04:59 +0100

    Support for koala-framework 4.x

--- TextImageTeaser/Kwc/Teaser/Component.php
+++ TextImageTeaser/Kwc/Teaser/Component.php
@@ -1,16 +1,16 @@
 <?php
 class TextImageTeaser_Kwc_Teaser_Component extends Kwc_Abstract_Composite_Component
 {
-    public static function getSettings()
+    public static function getSettings($param = null)
     {
-        $ret = parent::getSettings();
+        $ret = parent::getSettings($param);
         $ret['componentName'] = trlKwfStatic('Teaser with Image');
         $ret['generators']['child']['component']['image'] = 'TextImageTeaser_Kwc_Teaser_Image_Component';
         $ret['generators']['child']['component']['text'] = 'Kwc_Basic_Text_Component';
         $ret['generators']['child']['component']['link'] = 'Kwc_Basic_LinkTag_Component';
         $ret['extConfig'] = 'TextImageTeaser_Kwc_Teaser_ExtConfig';
         $ret['ownModel'] = 'TextImageTeaser_Kwc_Teaser_Model';
-        $ret['cssClass'] = 'webStandard';
+        $ret['rootElementClass'] = 'kwfUp-webStandard';
 
         $ret['colors'] = array(
             'none' => trlKwfStatic('None'),
@@ -20,9 +20,9 @@ class TextImageTeaser_Kwc_Teaser_Component extends Kwc_Abstract_Composite_Compon
         return $ret;
     }
 
-    public function getTemplateVars()
+    public function getTemplateVars(Kwf_Component_Renderer_Abstract $renderer)
     {
-        $ret = parent::getTemplateVars();
+        $ret = parent::getTemplateVars($renderer);
         $ret['color'] = $this->getRow()->color;
         return $ret;
     }
